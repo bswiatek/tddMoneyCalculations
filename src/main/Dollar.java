3c0203c4968c6e458b63fdb774274444191a3bab
Bartłomiej Świątek <emini1@swiatek.biz>
2017-02-05 16:12:06 +0100

    degenerated objects

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -7,7 +7,7 @@ public class Dollar {
     public Dollar(int amount) {
         this.amount = amount;
     }
-    void times(int multiplier) {
-        amount *= 2;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
