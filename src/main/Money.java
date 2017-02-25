dd722c5909c4b308c66988e292b0af0cb6292d86
Bartłomiej Świątek <emini1@swiatek.biz>
2017-02-25 12:53:29 +0100

    remove dollar & franc class

--- src/main/Money.java
+++ src/main/Money.java
@@ -1,6 +1,6 @@
 package main;
 
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
@@ -11,20 +11,25 @@ abstract class Money {
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
     protected String currency() {
         return currency;
-    };
+    }
+    public String toString() {
+        return amount + " " + currency;
+    }
 }
