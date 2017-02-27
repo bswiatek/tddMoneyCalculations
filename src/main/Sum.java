9759be80e31891c4a41a1db56460f8bd37a39d18
Bartłomiej Świątek <emini1@swiatek.biz>
2017-02-27 17:50:28 +0100

    sum class

---
+++ src/main/Sum.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Sum implements Expression{
+    Sum(Money augend, Money addend){
+        this.augend = augend;
+        this.addend = addend;
+    }
+    Money augend;
+    Money addend;
+
+    public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+}
