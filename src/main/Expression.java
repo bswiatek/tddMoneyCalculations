bbda9171b6791a4db6cd5bd029b4b375acddb132
Bartłomiej Świątek <emini1@swiatek.biz>
2017-03-04 17:07:22 +0100

    Expression plus

--- src/main/Expression.java
+++ src/main/Expression.java
@@ -2,4 +2,5 @@ package main;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+    Expression plus(Expression addend);
 }
--- src/main/Sum.java
+++ src/main/Sum.java
@@ -1,15 +1,20 @@
 package main;
 
 public class Sum implements Expression{
-    Sum(Money augend, Money addend){
+    Sum(Expression augend, Expression addend){
         this.augend = augend;
         this.addend = addend;
     }
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
     public Money reduce(Bank bank, String to) {
-        int amount = augend.amount + addend.amount;
+        int amount = augend.reduce(bank, to).amount
+                   + addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend) {
+        return null;
     }
 }
